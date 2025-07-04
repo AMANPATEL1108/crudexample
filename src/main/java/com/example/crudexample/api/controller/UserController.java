955a8d42fa733459b06c89d01013285801893316
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-07-04 09:53:04 +0530

    Nothing

--- src/main/java/com/example/crudexample/api/controller/UserController.java
+++ src/main/java/com/example/crudexample/api/controller/UserController.java
@@ -4,13 +4,13 @@ package com.example.crudexample.api.controller;
 import com.example.crudexample.api.model.User;
 import com.example.crudexample.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired
